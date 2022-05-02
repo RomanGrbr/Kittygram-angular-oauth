f134579924ea8a2e631f231cae6a71c80b1bf14b
Роман <romangrbr@gmail.com>
2022-05-02 10:55:56 +0300

    readme

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('register/', views.register),
     path('token/', views.token),
     path('token/refresh/', views.refresh_token),
-    path('token/revoke/', views.revoke_token),
+    path('token/revoke/token/revoke/', views.revoke_token),
 ]
